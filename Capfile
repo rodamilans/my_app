4be6757c30089392eaeb3360e6727c686728c5c7
Charles Rodamilans <charles.rodamilans@gmail.com>
2023-04-19 00:59:22 -0300

    Fix bundler issue

--- Capfile
+++ Capfile
@@ -38,7 +38,8 @@ require 'capistrano/passenger'
 require 'capistrano/rbenv'
 
 set :rbenv_type, :user
-set :rbenv_ruby, '3.2.2'
+# set :rbenv_ruby, '3.2.2'
+set :rbenv_ruby, '3.0.0'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.17.2"
 # set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
